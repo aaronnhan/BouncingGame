f56f43262813243ddf80b6642424ba46b7c3042c
Aaron Nhan <aaronnhan11@gmail.com>
2018-03-21 14:22:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.button import Button
 import time
+from kivy.core.image import Image as CoreImage
+from kivy.graphics import Color, Rectangle
 from kivy.app import App
 from kivy.uix.image import Image
 from kivy.uix.popup import Popup
@@ -16,10 +18,8 @@ from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty, StringProperty
 from math import sqrt, sin, cos, atan, pi
 import sys
-from kivy.clock import Clock
-from kivy.uix.screenmanager import ScreenManager, Screen
-#Window.size = (800, 400)
-#Window.size = (100, 288)
+from kivy.clock import Clock, partial
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition, FadeTransition
 Builder.load_string("""
 <Menu>:
     ScreenManager:
@@ -29,6 +29,18 @@ Builder.load_string("""
             id: title
             name: "title"
             size: root.size
+            Image: 
+                keep_ratio: False
+                allow_stretch: True
+                source: "dirt.png"
+            AnchorLayout:
+                anchor_x: "center"
+                anchor_y: "top"
+                Image:
+                    size_hint: (1,.6)
+                    source: "menu.png"
+                    center: (root.width/2, root.height/2)
+                    keep_ratio: False
             AnchorLayout:
                 anchor_x: "center"
                 anchor_y: "bottom"
@@ -38,11 +50,11 @@ Builder.load_string("""
                     Button:
                         size_hint: (1,.5)
                         text: "Play"
-                        on_press: root.ids["sm"].current = "levels"
+                        on_release: root.ids["sm"].current = "levels"
                     Button:
                         size_hint: (1,.5)
                         text: "Credits"
-                        on_press: root.ids["sm"].current = "credits"
+                        on_release: root.ids["sm"].current = "credits"
         Screen:
             id: levels
             name: "levels"
@@ -54,7 +66,7 @@ Builder.load_string("""
                     BoxLayout:
                         size_hint_y: None
                         orientation: "vertical"
-                        id:box
+                        id: box
         Screen:
             id: credits
             name: "credits"
@@ -73,7 +85,7 @@ Builder.load_string("""
                     Button:
                         text: "Back"
                         size_hint: (1,.2)
-                        on_press: root.ids["sm"].current = "title"
+                        on_release: root.ids["sm"].current = "title"
         Screen:
             id:game
             name: "game"
@@ -81,6 +93,31 @@ Builder.load_string("""
                 id: back_butt
                 anchor_x: "center"
                 anchor_y: "bottom"
+        Screen:
+            name: "white"
+            Image:
+                source: "white.jpeg"
+                allow_stretch: True
+                keep_ratio: False
+        Screen:
+            id: letter_screen
+            name: "letter"
+            Image: 
+                source: "parchment.png"
+                size_hint: (1,1)
+                allow_stretch: True
+                keep_ratio: False
+            Label:
+                size_hint: (1,1)
+                markup: True
+                id: letter
+<Intro>:
+    ScrollView:
+        size_hint: (.75, 1)
+        BoxLayout:
+            size_hint_y: None
+            orientation: "vertical"
+            id: box
 <FinishLayout>:
     size_hint: (1,1)
     pos: root.pos
@@ -123,7 +160,9 @@ Builder.load_string("""
     Image:
         id: player
         size_hint: (.1, .1)
-#        source: "player.png"
+        source: "boy.png"
+        allow_stretch: True
+        keep_ratio: False
         pos: self.pos
         size: root.mySize
 <Node>:
@@ -132,6 +171,8 @@ Builder.load_string("""
 #        source: "background.png"
         pos: self.pos
         size:root.mySize
+        allow_stretch: True
+        #keep_ratio: False
 <Laser>:
     Image:
         id: laser
@@ -146,12 +187,33 @@ Builder.load_string("""
         pos: self.pos
         size: 10,10
 <Wall>:
+    canvas:
     Image:
         id: wall
 #        source: "line.png"
         pos: self.pos
+<Enter_Animation>:
+    BoxLayout:
+        size: (10,10)
+        size_hint: (1,1)
+        id: animation
+        Image:
+            size: (100,100)
+            size_hint: (1,1)
+            id: boy
+            keep_ratio: False
+            allow_stretch: True
+            myRotation: 45
+            source: "boy.png"
+            canvas.before:
+                PushMatrix
+                Rotate:
+                    angle: self.myRotation
+                    origin: self.center
+            canvas.after:
+                PopMatrix
 """)
-#Window.size = (592, 288)
+Window.size = (592, 288)
 ratio_y = Window.height/144.
 ratio_x = Window.width/296.
 class levelBuilder():
@@ -274,7 +336,7 @@ class Player(Widget):
             self.ids["player"].x +=self.velocity[0]*ratio_x
             self.ids["player"].y +=self.velocity[1]*ratio_y
         else:
-            self.angle += .15*self.direction
+            self.angle += .1*self.direction
             self.ids["player"].center_x = self.node.ids["node"].center_x + self.node.radius*cos(self.angle)
             self.ids["player"].center_y = self.node.ids["node"].center_y + self.node.radius*sin(self.angle)
     def calcAngle(self):
@@ -305,17 +367,21 @@ class Player(Widget):
                     pow(self.ids["player"].center_x - self.nodelist[x].ids["node"].center_x, 2))
             if (distance <= self.nodelist[x].radius and self.nodelist[x] != self.last_node):
                 self.node = self.nodelist[x]
+                self.node.transform("current")
+                if self.last_node:
+                    self.last_node.transform("revert")
                 self.last_node = self.nodelist[x]
                 if self.node == self.finish_node:
                     self.finished = True
                     print "Finished"
         for x in range(len(self.laserlist)):
-            if self.laserlist[x].ids["laser"].collide_point(self.ids["player"].x, self.ids["player"].y):
+            if x<len(self.laserlist) and self.laserlist[x].ids["laser"].collide_point(self.ids["player"].x, self.ids["player"].y):
                 self.parent.ids["sm"].current = "0"
-                
+                if self.last_node:
+                    self.last_node.transform("revert")
                 self.restart()
         for x in range(len(self.walllist)):
-            if self.walllist[x].ids["wall"].collide_point(self.ids["player"].x, self.ids["player"].y) and (time.time()-self.lastWall[1]>.1): # COLLIDE
+            if x<len(self.walllist) and self.walllist[x].ids["wall"].collide_point(self.ids["player"].x, self.ids["player"].y) and (time.time()-self.lastWall[1]>.1): # COLLIDE
                 if self.walllist[x].angle == 90:
                     self.velocity[0]*=-1
                     self.lastWall[0] = self.walllist[x]
@@ -342,7 +408,7 @@ class Player(Widget):
                 xDir = 1
             else: 
                 xDir = -1
-            self.velocity = [abs(3*x/ratio)*xDir, abs(3*y/ratio)*yDir]
+            self.velocity = [abs(2*x/ratio)*xDir, abs(2*y/ratio)*yDir]
             self.node = None
     def restart(self, *args):
         self.last_node = None
@@ -359,15 +425,20 @@ class Player(Widget):
         self.ids["player"].center = (x,y)
 class Node(Widget):
     def __init__(self,x,y,radius, **kwargs):
-        self.mySize = (4*ratio_x, 4*ratio_y)
+        self.mySize = [4*ratio_x, 4*ratio_y]
         super(Node, self).__init__(**kwargs)
         self.radius = radius*ratio_x
         self.ids["node"].center = (x*ratio_x, y*ratio_y)
         self.used = False
-#class Obstacle(Widget):
-#    def __init__(self,x, y, **kwargs):
-#        super(Obstacle, self).__init__(**kwargs)
-#        self.ids["obstacle"].center = (x*window_ratio_x, y*window_ratio_y)
+    def transform(self, img):
+        if img == "end":
+            hole = Image(center = self.ids["node"].center, source = "hole.png")
+            #self.ids["node"].source = "hole.png"
+            self.parent.add_widget(hole)
+        if img == "current":
+            self.ids["node"].source = "box.png"
+        if img == "revert":
+            self.ids["node"].source = "white.jpeg"
 class Wall(Widget):
     def __init__(self,x, y, size_x, size_y, angle, **kwargs):
         self.angle = angle
@@ -375,6 +446,20 @@ class Wall(Widget):
         self.ids["wall"].width = size_x*ratio_x
         self.ids["wall"].height = ratio_y*size_y
         self.ids["wall"].center = (x*ratio_x, y*ratio_y)
+        self.ids["wall"].source = "white.jpeg"
+        self.ids["wall"].keep_ratio = False
+        self.ids["wall"].allow_stretch = True
+        with self.canvas:
+            Color(.4, .4, .4, 1)
+            texture = CoreImage("box.png").texture
+            texture.wrap = 'repeat'
+            #nx = float(self.width) / texture.width * ratio_x
+            #ny = float(self.height) / texture.height * ratio_y
+            nx = size_x/5
+            ny = size_y/5
+            Rectangle(size=(size_x*ratio_x,size_y*ratio_y), pos=(x*ratio_x-size_x*ratio_x/2,y*ratio_y-size_y*ratio_y/2),texture=texture,
+                      tex_coords=(0, 0, nx, 0, nx, ny, 0, ny))
+        #self.canvas.add()
 class Laser(Widget):
     def __init__(self,x, y, size_x, size_y, **kwargs):
         super(Laser, self).__init__(**kwargs)
@@ -425,30 +510,39 @@ class Cube(Widget):
     def __init__(self, level,**kwargs):
         super(Cube, self).__init__(**kwargs)
         self.builder = levelBuilder()
+        self.level = level
         self.face_guide = [[3,2,1,4],[0,2,5,4],[0,3,5,1],[0,4,5,2],[0,1,5,3],[1,2,3,4]]
         self.faces = self.builder.return_level(level) # list of lists for gamelayout (nodelist, etc.)
         for x in range(2):
             self.ids["layout" + str(x)].set_lists(self.faces[x][0], self.faces[x][1],self.faces[x][2])
-       # for x in self.faces:
-            #self.ids["layout" + str(x+1)].loadLevel()
         self.ids["layout0"].loadLevel()
         self.myPlayer = Player(self.faces[0][0], self.faces[0][1],self.faces[0][2])
         self.myPlayer.start_center = self.myPlayer.nodelist[0].ids["node"].center
         self.myPlayer.finish_node = self.myPlayer.nodelist[-1]
+        self.myPlayer.finish_node.transform("end")
         self.myPlayer.ids["player"].center = self.myPlayer.start_center
         self.add_widget(self.myPlayer)
-        Clock.schedule_interval(self.update, 1/40.)
+        Clock.schedule_interval(self.update, 1/60.)
         self.dir_list = ["down","left","up","right"]
     def update(self, dt):
         self.myPlayer.update(dt)
         if self.myPlayer.finished:
             Clock.unschedule(self.update)
             self.finish_layout = BoxLayout()
-            self.finish_layout.add_widget(Button(text = "Restart", on_press = self.restart_level, size_hint = (1, 1)))
-            self.finish_layout.add_widget(Button(text = "Levels", on_press = self.select_level, size_hint = (1, 1)))
-            self.finish_layout.add_widget(Button(text = "Next", on_press = self.next_level, size_hint = (1, 1)))
+            self.finish_layout.add_widget(Button(text = "Restart", on_release = self.restart_level, size_hint = (1, 1)))
+            self.finish_layout.add_widget(Button(text = "Levels", on_release = self.select_level, size_hint = (1, 1)))
+            self.finish_layout.add_widget(Button(text = "Next", on_release = self.next_level, size_hint = (1, 1)))
             self.finish_popup = Popup(content = self.finish_layout, title = "Menu", size_hint= (.9,.9))
             self.finish_popup.open()
+            with open("current.txt","r") as file: 
+                self.level_holder = int(file.read())
+                file.close()
+            if  self.level_holder < self.level:
+                with open("current.txt", "w") as file:
+                    file.write(str(self.level))
+                    self.parent.parent.parent.unlocked_level = self.level
+                    self.parent.parent.parent.make_menu()
+                    file.close()
     def restart_level(self, obj):
         self.finish_popup.dismiss()
         self.parent.parent.parent.restart_level()
@@ -458,7 +552,6 @@ class Cube(Widget):
     def next_level(self, obj):
         self.finish_popup.dismiss()
         self.parent.parent.parent.next_level()
-
     def change_face(self, direction):
         self.ids["sm"].transition.direction = self.dir_list[direction]
         self.ids["sm"].current = str(1- int(self.ids["sm"].current))
@@ -480,15 +573,54 @@ class Cube(Widget):
 class Menu(Widget):
     def __init__(self,**kwargs):
         super(Menu, self).__init__(**kwargs)
-        for x in range(len(levelBuilder().find_levels())):
-            self.ids["box"].add_widget(Button(text = str(x+1), on_press = self.press))
+        self.ids["letter"].text = """[color=000000]Dear Son\n
+                If you're reading this, I'm glad you got this message.\n
+                As I was taking my morning stroll, I fell down 15 consecutive holes.\n
+                My vision isn't what it used to be.\n
+                I would be much obliged if you could rescue me before dinner.\n
+                Love, Dad"""
+        self.ids["letter_screen"].add_widget(Button(background_color = (1,0,0,0), on_release = partial(self.press_setup2, 1)))
+        with open("current.txt","r") as file:
+            self.unlocked_level = int(file.read())
+            file.close()
+        self.make_menu()
         self.ids["box"].height = len(self.ids["box"].children)*self.ids["box"].children[0].height
         self.current_level = 0
+    def make_menu(self):
+        self.ids["box"].clear_widgets()
+        print self.unlocked_level
+        for x in range(self.unlocked_level+1):
+            self.ids["box"].add_widget(Button(text = str(x+1), on_release = self.press))
     def press(self, obj):
+        self.ids["sm"].current = "game"
+        if int(obj.text) == 1:
+            self.ids["sm"].current = "letter"
+        else:
+            self.ids["game"].clear_widgets()
+            self.ids["game"].add_widget(Enter_Animation())
+            Clock.schedule_once(self.fade, 1.95)
+            Clock.schedule_once(partial(self.press_setup, obj), 2.1)
+    def press_setup(self, obj, dt):
         self.ids["game"].clear_widgets()
         self.ids["game"].add_widget(Cube(int(obj.text)))
         self.current_level = int(obj.text)
-        self.ids["sm"].current = "game"
+        Clock.schedule_once(partial(self.fade), 1)
+    def press_setup2(self, lvl, obj):
+        print lvl
+        self.ids["game"].clear_widgets()
+        self.ids["game"].add_widget(Cube(lvl))
+        self.current_level = lvl
+        self.ids["sm"].transition = FadeTransition()
+        self.ids["sm"].current = "white"
+        Clock.schedule_once(self.fade, .9)
+    def fade(self, dt):
+        if self.ids["sm"].current == "game":
+            self.ids["sm"].transition = FadeTransition()
+            self.ids["sm"].duration = 4
+            self.ids["sm"].current = "white"
+        else:
+            self.ids["sm"].current = "game"
+            self.ids["sm"].transition = SlideTransition()
     def back(self):
         self.ids["sm"].current = "title"
     def restart_level(self):
@@ -499,9 +631,33 @@ class Menu(Widget):
     def next_level(self):
         self.current_level +=1
         if self.current_level > len(levelBuilder().find_levels()):
-            self.current_level = 1
+            self.current_level = 1 #LAST LEVEL
         self.ids["game"].clear_widgets()
-        self.ids["game"].add_widget(Cube(self.current_level))
+        self.ids["game"].add_widget(Enter_Animation())
+        Clock.schedule_once(self.fade, 1.95)
+        Clock.schedule_once(partial(self.press_setup, Button(text = str(self.current_level))), 2.1)
+    def intro_release(self, obj):
+        self.intro_popup.dismiss()
+        self.ids["game"].clear_widgets()
+        self.ids["game"].add_widget(Enter_Animation())
+        Clock.schedule_once(self.fade, 1.95)
+        Clock.schedule_once(partial(self.press_setup, Button(text = "1")), 2.1)
+class Enter_Animation(Widget):
+    def __init__(self, **kwargs):
+        super(Enter_Animation, self).__init__(**kwargs)
+        self.animate()
+    def animate(self):
+        Clock.schedule_interval(self.animation, 1/60.)
+        Clock.schedule_once(self.unschedule, 2.5)
+    def unschedule(self, dt):
+        Clock.unschedule(self.animation)
+    def animation(self, dt):
+        self.ids["boy"].myRotation-=4
+        self.ids["animation"].height=self.ids["boy"].myRotation/10. + 40
+        self.ids["animation"].width=self.ids["boy"].myRotation/10. +40
+        self.ids["animation"].center_x = Window.width/2 + (Window.width/2 + Window.width/1200.*self.ids["boy"].myRotation)*sin(self.ids["boy"].myRotation/90.)
+        self.ids["animation"].center_y = Window.height/2 + (Window.height/2 + Window.height/1200.*self.ids["boy"].myRotation)*cos(self.ids["boy"].myRotation/90.)
+
 class TestApp(App):
     def build(self):
         return Menu()
